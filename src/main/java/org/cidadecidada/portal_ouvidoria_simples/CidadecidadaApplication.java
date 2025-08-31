b9340a81902a844d3f2bd9479f88802d68f790fa
Guimaraes Neto <guimaraesneto@gmail.com>
2025-08-31 17:11:25 -0300

    Subindo projeto

---
+++ src/main/java/org/cidadecidada/portal_ouvidoria_simples/CidadecidadaApplication.java
@@ -0,0 +1,13 @@
+package org.cidadecidada.portal_ouvidoria_simples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadecidadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadecidadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cidadecidada/portal_ouvidoria_simples/CidadecidadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cidadecidada.portal_ouvidoria_simples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadecidadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
